72fa45302d602be2d55389b3aad750dbb87c8430
nia45 <nia45654645@gmail.com>
2026-09-20 23:04:28 +0400

    add footer component and assets

--- src/pages/home/home-page.ts
+++ src/pages/home/home-page.ts
@@ -2,6 +2,7 @@ import { createHero } from './hero/hero'
 import { createNewGames } from '../../components/new-games/new-games'
 import { createTopPlayers } from '../../components/top-players/top-players'
 import { createDeveloperCta } from '../../components/developer-cta/developer-cta'
+import { createFooter } from '../../components/footer/footer'
 
 export const createHomePage = (): HTMLElement => {
   const main = document.createElement('main')
@@ -11,6 +12,7 @@ export const createHomePage = (): HTMLElement => {
     createNewGames(),
     createTopPlayers(),
     createDeveloperCta(),
+    createFooter(),
   )
 
   return main
