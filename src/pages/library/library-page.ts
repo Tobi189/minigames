73cae72ab7c1cdf41bd5454eef3a1e93903fd4c7
nia45 <nia45654645@gmail.com>
2026-09-26 10:37:05 +0400

    feat: add library introduction

--- src/pages/library/library-page.ts
+++ src/pages/library/library-page.ts
@@ -1,6 +1,10 @@
+import { createLibraryIntro } from '../../components/library-intro/library-intro'
+
 export const createLibraryPage = (): HTMLElement => {
   const page = document.createElement('div')
   page.className = 'library-page'
+
+  page.append(createLibraryIntro())
 
   return page
 }
